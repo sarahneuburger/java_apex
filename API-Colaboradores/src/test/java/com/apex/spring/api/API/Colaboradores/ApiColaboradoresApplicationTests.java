f1cfe8bb298c78ad8e9ea07ea5bac378fe9f7b27
sarahneuburger <61321914+sarahneuburger@users.noreply.github.com>
2021-02-09 14:23:53 -0300

    Aula de Spring - API

---
+++ API-Colaboradores/src/test/java/com/apex/spring/api/API/Colaboradores/ApiColaboradoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.spring.api.API.Colaboradores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiColaboradoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
